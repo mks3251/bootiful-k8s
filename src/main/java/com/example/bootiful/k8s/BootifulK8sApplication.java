693d6c4285af8674810d76bffe7f0b9024145a1d
Matthew Sloan <msloan@Matthews-MacBook-Pro.local>
2021-08-09 16:05:43 -0500

    initial commit

---
+++ src/main/java/com/example/bootiful/k8s/BootifulK8sApplication.java
@@ -0,0 +1,23 @@
+package com.example.bootiful.k8s;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
+import org.springframework.boot.cloud.CloudPlatform;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BootifulK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulK8sApplication.class, args);
+	}
+
+	@Bean
+	@ConditionalOnCloudPlatform(CloudPlatform.KUBERNETES)
+	InitializingBean initializingBean() {
+		return () -> System.out.println("Hello containers from " + BootifulK8sApplication.class.getName());
+	}
+
+}
---
+++ src/test/java/com/example/bootiful/k8s/BootifulK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootiful.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
